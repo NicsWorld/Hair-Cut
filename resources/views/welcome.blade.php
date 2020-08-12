21ba1edcaeb61021c868c3c621ab63701fb32e96
nic <nicwoods63@gmail.com>
2020-08-12 09:49:27 -0500

    add homepage design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.app')
     @section('content')
-    @auth
-      <div id="users-list"></div>
-    @endauth
+    <section class='welcome-hero'>
+      <div>Cut My Hair Pls</div>
+    </section>
     <ul class='ws-1'>
       <a href='/login'><li id="client-welcome"></li></a>
       <a href='/login'><li id="barber-welcome"></li></a>
     </ul>
-
+    @auth
+      <div id="users-list"></div>
+    @endauth
     @endsection
